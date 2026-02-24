5f35979b881071782bd9f550581b68d9b9af86f1
hamooziman <165272290+hamooziman@users.noreply.github.com>
2026-02-23 23:56:04 -0700

    Update PickupTruck.java

--- Assignment 2/src/ProblemDomain/PickupTruck.java
+++ Assignment 2/src/ProblemDomain/PickupTruck.java
@@ -1,35 +1,36 @@
 package ProblemDomain;
 
 public class PickupTruck extends Vehicle {
-    private String cargoBeds;
-    private int cargoCapacity;
+	
+	private String cargoBeds;
+	private int cargoCapacity;
+	
+	public PickupTruck(long carID, String vehicleType, String subType, double fuel, int seats, int year, String drivetrain, double price, int quantity, String cargoBeds, int cargoCapacity) {
+		super(carID, vehicleType, subType, speed, fuel, seats, year, drivetrain, price, quantity);
+		
+		this.cargoBeds = cargoBeds;
+		this.cargoCapacity = cargoCapacity;
+	}
+	
+	private String getFormattedCargoBeds() {
+		switch (cargoBeds.toUpperCase()) {
+		case "SB": return "Short Bed";
+		case "EB": return "Extended Bed";
+		case "DB": return "Dump Bed";
+		default: return cargoBeds;
+		}
+		
+	}
+	
+	@Override
+	public String toFileString() {
+		return super.toFileString() + ";" + cargoBeds + ";" + cargoCapacity;
 
-    public PickupTruck(long carID, String vehicleType, String subType, int speed, double fuel, int seats, int year, String drivetrain, int price, int quantity, String cargoBeds, int cargoCapacity) {
-        super(carID, vehicleType, subType, speed, fuel, seats, year, drivetrain, price, quantity);
-        this.cargoBeds = cargoBeds;
-        this.cargoCapacity = cargoCapacity;
-    }
+	}
+	
+	@Override
+	public String toString() {
+		return super.toString() + "Cargo Capacity:\t" + cargoCapacity + "\nCargo Beds:\t" + getFormattedCargoBeds() + "\n";
+	}
 
-    public String getCargoBeds() { 
-        return cargoBeds; 
-    }
-
-    private String getFormattedCargoBeds() {
-        switch (cargoBeds.toUpperCase()) {
-            case "SB": return "Short Bed";
-            case "EB": return "Extended Bed";
-            case "DB": return "Dump Bed";
-            default: return cargoBeds;
-        }
-    }
-
-    @Override
-    public String toFileString() {
-        return super.toFileString() + ";" + cargoBeds + ";" + cargoCapacity;
-    }
-
-    @Override
-    public String toString() {
-        return super.toString() + "Cargo Capacity:\t" + cargoCapacity + "\nCargo Beds:\t" + getFormattedCargoBeds() + "\n";
-    }
 }
